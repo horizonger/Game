92ab4af63bb12673a33a9977851f3eeae8273132
ufuk <70153893+horizonger@users.noreply.github.com>
2020-10-21 19:55:07 +0300

    Create V00 episodeone.py

---
+++ V00 episodeone.py
@@ -0,0 +1,14 @@
+def welcome(start):
+    print(f"""
+    welcome the game {start}, 
+    we are waiting  you since you are looking this game.
+    you're in the forest and you see so many different things(animals,trees,insects) 
+    that you've never seen before in your life.
+    what do you want to do.
+                you can "look around", or 
+                you can "walk into the jungle, or 
+                you can sit here and just cry, or
+                just write random and leave the choice to us
+    
+    
+    """)
