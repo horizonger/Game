f7b5c5eca1ba2b600f5bc69de1f3174fd8048a8d
ufuk <70153893+horizonger@users.noreply.github.com>
2020-10-21 19:56:09 +0300

    Create V00 main.py

---
+++ V00 main.py
@@ -0,0 +1,42 @@
+rom GameIGuess import Character
+from GameIGuess import episode1
+
+# from GameIGuess import EpisodeOneObject
+
+
+def episode_one(episode):
+    return episode1.welcome(episode)
+
+
+print("""
+
+WELCOME THE GAME
+JUST CHOOSE ONE CHARACTER
+Character:
+        1:WARRIOR
+        2:ORDINARY PERSON
+        3:VAMPIRE
+        4:WITCH
+""")
+
+while True:
+    first_Character = int(input(">>>"))
+
+    if first_Character == 1:
+        print(Character.old_warrior.character_info())
+        character1 = Character.old_warrior.attributes()
+        episode_one(character1)
+    elif first_Character == 2:
+        print(Character.regular_human.character_info())
+        character2 = Character.old_warrior.attributes()
+        episode_one(character2)
+
+    elif first_Character == 3:
+        print(Character.young_Vampire.character_info())
+        character3 = Character.old_warrior.attributes()
+        episode_one(character3)
+
+    elif first_Character == 4:
+        print(Character.old_which.character_info())
+        character4 = Character.old_warrior.attributes()
+        episode_one(character4)
