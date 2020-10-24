cb92d28d88e7d25af67c01a25a6fb5f58a12c861
ufuk <70153893+horizonger@users.noreply.github.com>
2020-10-24 20:14:56 +0300

    Update V00 EpisodeOneObject.py

--- V00 EpisodeOneObject.py
+++ V00 EpisodeOneObject.py
@@ -36,6 +36,59 @@ class FruitTree(Forest):
                  ):
         super().__init__(name, length, width, age, weakness, kind)
         self.number_of_trees = number_of_trees
+        
+        
+        
+class Deadtrees_c(Forest):
+    def __init__(self,
+                 name="unknown",
+                 length="3m",
+                 width="1m",
+                 age="5",
+                 weakness="fire,cold and idiot people ",
+                 kind="unknown",
+                 number_of_DeadliestPlants="2000",
+                 ):
+        super().__init__(name, length, width, age, weakness, kind)
+        self.number_of_DeadliestPlants= number_of_trees
+        
+        
+       
+class DeadliestPlants_c(Forest):
+    def __init__(self,
+                 name="unknown",
+                 length="3m",
+                 width="1m",
+                 age="5",
+                 weakness="fire,cold and idiot people ",
+                 kind="unknown",
+                 number_of_DeadliestPlants_c="1055",
+                 ):
+        super().__init__(name, length, width, age, weakness, kind)
+        self.number_of_trees = number_of_trees
+    
+    
 
+class Flowers_c(Forest):
+    def __init__(self,
+                 name="unknown",
+                 length="3m",
+                 width="1m",
+                 age="5",
+                 weakness="fire,cold and idiot people ",
+                 kind="unknown",
+                 number_of_Flowers_c="12530",
+                 ):
+        super().__init__(name, length, width, age, weakness, kind)
+        self.number_of_DeadliestPlants_c = number_of_trees    
+   
+           
+
+        
+
+        
 
 trees = FruitTree()
+deadtrees = Deadtrees_c()
+DeadliestPlants = DeadliestPlants_c()
+flowers = Flowers_c()
