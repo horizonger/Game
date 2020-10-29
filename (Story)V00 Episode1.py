b99b0efa1fe3acb8afb9ff6fe843c0f040cebd7d
UFUK GİRİŞ <70153893+horizonger@users.noreply.github.com>
2020-10-29 19:37:24 +0300

    Update (Story)V00 Episode1.py

--- (Story)V00 Episode1.py
+++ (Story)V00 Episode1.py
@@ -2,7 +2,7 @@ def welcome(start):
     print(f"""
     welcome the game {start}, 
     we are waiting  you since you are looking this game.
-    you're in the forest and you see so many different things(animals,trees,insects) 
+    you're in the forest and you see so many different things(animals,trees, etc) 
     that you've never seen before in your life.
     what do you want to do.
                 you can "look around", or 
