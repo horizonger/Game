3a0f8411b919761102acd445e89ae1c0de7f1b64
UFUK GİRİŞ <70153893+horizonger@users.noreply.github.com>
2020-11-19 20:33:09 +0300

    Update (Story)V00 Episode2.py

--- (Story)V00 Episode2.py
+++ (Story)V00 Episode2.py
@@ -1,1 +1,37 @@
+ef welcome_again(start):
+    print(f"""
+    welcome the game again {start}, 
+    
+    
+    """)
+    
+def second_choose(first_step):
+    print("""
+    Now, 
+    
+    """)
+    
+    
 
+    
+def e(animal):
+    print("""
+    
+    
+    
+    """)
+    
+    
+    
+def a(fruit):
+    print("""
+    
+    
+    
+    """)
+   
+def end(endfirstpart):
+    print("""
+    
+    
+    """)
