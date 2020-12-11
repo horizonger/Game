3b0d646427fe30f51b04aa15d018d281fea9fee0
UFUK GİRİŞ <70153893+horizonger@users.noreply.github.com>
2020-12-11 19:25:30 +0300

    Update (Story)V00 Episode2.py

--- (Story)V00 Episode2.py
+++ (Story)V00 Episode2.py
@@ -1,8 +1,10 @@
 def welcome_again(start):
     print(f"""
     welcome the game again {start}, 
-    
-    
+    now you're in this city, 
+    this city is full of terrible people    
+    be more careful than before, 
+    because now the main problem is humans.
     """)
     
 def second_choose(first_step):
